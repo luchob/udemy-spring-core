61ae805e5d26c5227cc3e3ccd4d2b8e49325afa7
luchob <lachezar.balev@gmail.com>
2023-07-15 14:43:54 +0300

    Jva based config.

---
+++ di-java/src/main/java/eu/balev/Main.java
@@ -0,0 +1,16 @@
+package eu.balev;
+
+import eu.balev.student.StudentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+        StudentService studentService = ctx.getBean(StudentService.class);
+
+        System.out.println(studentService.findYoungestStudents());
+    }
+}
---
+++ di-java/src/main/java/eu/balev/student/StudentServiceImpl.java
@@ -0,0 +1,73 @@
+package eu.balev.student;
+
+import eu.balev.student.model.Student;
+import eu.balev.student.repository.StudentRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(List<StudentRepository> studentRepositories) {
+        this.studentRepository = new CompositeStudentReposisotry(studentRepositories);
+    }
+
+    @Override
+    public Set<Student> findYoungestStudents() {
+        var sorted =  studentRepository
+                .getAllStudents()
+                .stream()
+                .sorted(Comparator.comparing(Student::birthDay).reversed())
+                .toList();
+
+        if (sorted.isEmpty()) {
+            return Set.of();
+        } else {
+            LocalDate lastBirthDay = sorted.get(0).birthDay();
+
+            return sorted
+                    .stream()
+                    .filter(s -> s.birthDay().equals(lastBirthDay))
+                    .collect(Collectors.toSet());
+        }
+    }
+
+    @Override
+    @PostConstruct
+    public void init() {
+        System.out.println("We have " + studentRepository.count() + " student(s).");
+    }
+
+
+    static class CompositeStudentReposisotry implements StudentRepository {
+
+        private final List<StudentRepository> studentRepositories;
+
+        CompositeStudentReposisotry(List<StudentRepository> studentRepositories) {
+            this.studentRepositories = studentRepositories;
+        }
+
+        @Override
+        public List<Student> getAllStudents() {
+            return studentRepositories
+                    .stream()
+                    .flatMap(r -> r.getAllStudents().stream())
+                    .toList();
+        }
+
+        @Override
+        public int count() {
+            return studentRepositories.
+                    stream().
+                    mapToInt(StudentRepository::count).sum();
+        }
+    }
+}
