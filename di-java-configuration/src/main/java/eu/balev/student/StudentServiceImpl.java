52f6738235329a336cadd59ce8379e6e087141af
luchob <lachezar.balev@gmail.com>
2023-07-20 20:03:13 +0300

    Remove needless config

--- di-java-configuration/src/main/java/eu/balev/student/StudentServiceImpl.java
+++ di-java-configuration/src/main/java/eu/balev/student/StudentServiceImpl.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Service
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
