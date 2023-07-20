c02d5132e71b043e155b4dc2319010309b0e2d89
luchob <lachezar.balev@gmail.com>
2023-07-20 19:31:35 +0300

    DI java stereotypes

---
+++ di-java-stereotypes/src/main/java/eu/balev/Main.java
@@ -0,0 +1,17 @@
+package eu.balev;
+
+import eu.balev.student.StudentService;
+import eu.balev.student.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        StudentService studentService = ctx.getBean(StudentService.class);
+
+        System.out.println(studentService.findYoungestStudents());
+    }
+}
---
+++ di-java-stereotypes/src/main/java/eu/balev/student/StudentServiceImpl.java
@@ -0,0 +1,47 @@
+package eu.balev.student;
+
+import eu.balev.student.model.Student;
+import eu.balev.student.repository.StudentRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public Set<Student> findYoungestStudents() {
+        var sorted =  studentRepository
+                .getAllStudents()
+                .stream()
+                .sorted(Comparator.comparing(Student::birthDay).reversed())
+                .toList();
+
+        if (sorted.isEmpty()) {
+            return Set.of();
+        } else {
+            LocalDate lastBirthDay = sorted.get(0).birthDay();
+
+            return sorted
+                    .stream()
+                    .filter(s -> s.birthDay().equals(lastBirthDay))
+                    .collect(Collectors.toSet());
+        }
+    }
+
+    @Override
+    @PostConstruct
+    public void init() {
+        System.out.println("We have " + studentRepository.count() + " student(s).");
+    }
+}
