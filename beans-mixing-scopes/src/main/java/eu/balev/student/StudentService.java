27613f74ec4ff165eb6f93971e9d2581691570e1
luchob <lachezar.balev@gmail.com>
2025-01-15 17:39:40 +0200

    Mixing scopes

---
+++ beans-mixing-scopes/src/main/java/eu/balev/student/StudentService.java
@@ -0,0 +1,13 @@
+package eu.balev.student;
+
+import eu.balev.student.model.Student;
+import java.util.Set;
+
+public interface StudentService {
+
+    Set<Student> findYoungestStudents();
+
+    void init();
+
+    void destroy();
+}
