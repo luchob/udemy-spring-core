343e25ba020828a8a74934aa257f2d3caab5eec0
luchob <lachezar.balev@gmail.com>
2025-01-22 20:19:36 +0200

    Order of beans

---
+++ order-of-beans/src/main/java/eu/balev/Main.java
@@ -0,0 +1,16 @@
+package eu.balev;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(
+            "eu.balev"
+        );
+
+        ctx.registerShutdownHook();
+    }
+
+}
---
+++ order-of-beans/src/main/java/eu/balev/student/config/AppConfig.java
@@ -0,0 +1,10 @@
+package eu.balev.student.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+}
